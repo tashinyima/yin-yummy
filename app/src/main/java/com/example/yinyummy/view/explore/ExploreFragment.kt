b693c1631c696f9bfe61de5b284e9d33983e502f
tashi nyima <tenzinchoephel2050@gmail.com>
2023-01-04 10:18:25 -0500

    file package

--- app/src/main/java/com/example/yinyummy/view/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/yinyummy/view/explore/ExploreFragment.kt
@@ -1,4 +1,4 @@
-package com.example.yinyummy.view
+package com.example.yinyummy.view.explore
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,11 +15,11 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [OrderFragment.newInstance] factory method to
+ * Use the [ExploreFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 @AndroidEntryPoint
-class OrderFragment : Fragment() {
+class ExploreFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,26 +37,8 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OrderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OrderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
