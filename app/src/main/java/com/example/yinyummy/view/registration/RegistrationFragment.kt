4d00f44bb8256e88b18b2369eb90bdc534f7c5df
roshan2020 <indika.menikarachi@live.com>
2023-01-10 09:01:54 -0800

    I edited as much as I could but I have an issue with database edit

--- app/src/main/java/com/example/yinyummy/view/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/yinyummy/view/registration/RegistrationFragment.kt
@@ -1,20 +1,35 @@
 package com.example.yinyummy.view.registration
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
 import com.example.yinyummy.R
+import com.example.yinyummy.databinding.FragmentRegistrationBinding
+import com.example.yinyummy.view.login.LoginFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegistrationFragment : Fragment() {
 
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
+        }
+        _binding = FragmentRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, LoginFragment::class.java))
+        }
+        binding.txtHaveAcc.setOnClickListener {
+            startActivity(Intent(this, LoginFragment::class.java))
         }
     }
 
@@ -23,7 +38,8 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        _binding = FragmentRegistrationBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 
